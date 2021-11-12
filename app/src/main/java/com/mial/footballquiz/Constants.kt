2d2cdb5e595bba522ce5393f3e0a5885d0c33105
mialskywalker <kalqga123@gmail.com>
2021-11-12 14:40:11 +0200

    -up to que5 added

--- app/src/main/java/com/mial/footballquiz/Constants.kt
+++ app/src/main/java/com/mial/footballquiz/Constants.kt
@@ -11,20 +11,44 @@ object Constants {
         val questionsList = ArrayList<Question>()
 
         val que1 = Question(
-            1, "Which team won the Champions League in 2006/07?",
+            1, "Which club has won the most Champions League titles?",
             R.drawable.ic_flag_of_argentina,
-            "FC Barcelona", "AC Milan", "Manchester United", "FC Porto",
-            3
+            "FC Barcelona", "Real Madrid", "Bayern Munich", "AC Milan",
+            2
         )
         questionsList.add(que1)
 
         val que2 = Question(
-            2, "ASDASDQWEQWDASD?",
+            2, "When was the first Premier League played?",
             R.drawable.ic_flag_of_argentina,
-            "FC Barcelona", "AC Milan", "Manchester United", "FC Porto",
-            3
+            "1985/86", "1943/44", "1996/97", "1992/93",
+            4
         )
         questionsList.add(que2)
+
+        val que3 = Question(
+            3, "Which Barcelona player was jailed after it was found that his passport was fake?",
+            R.drawable.ic_flag_of_argentina,
+            "Lionel Messi", "Samuel Eto'o", "Dani Alves", "Ronaldinho",
+            4
+        )
+        questionsList.add(que3)
+
+        val que4 = Question(
+            4, "Which footballer had a galaxy named after him?",
+            R.drawable.ic_flag_of_argentina,
+            "Cristiano Ronaldo", "Diego Maradona", "Lionel Messi", "Pele",
+            1
+        )
+        questionsList.add(que4)
+
+        val que5 = Question(
+            5, "Which player won the World Cup Young Player award in 2018?",
+            R.drawable.ic_flag_of_argentina,
+            "Vinicius Jr", "Kylian Mbappe", "Erling Haaland", "Frenkie de Jong",
+            2
+        )
+        questionsList.add(que5)
         // TODO Questions
 
         return questionsList
